e617df2af565459ce098c8bbba4739b267e5d024
Benoit Roualec <benoit.roualec@gmail.com>
2019-02-13 14:19:48 +0100

    Done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,5 @@ class Restaurant < ApplicationRecord
   validates :category, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'],
                                     message: '%{value} is not a valid category' }, presence: true
   validates :address, presence: true
+  has_many :reviews, :dependent => :destroy
 end
